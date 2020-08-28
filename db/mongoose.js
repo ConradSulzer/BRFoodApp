500da866390d9e243748d3e3d69ffbd5d58ed855
Conrad Sulzer <sulzer.conrad@gmail.com>
2020-08-28 16:11:25 -0500

    key

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://csulzer:C0nrad11@gencluster-pnz8e.gcp.mongodb.net/restList2?retryWrites=true&w=majority', { 
+mongoose.connect(process.env.MONGODB_URL_TWO, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
